1adbc582fdfe66587f0ca5241c176784813b682b
Gabe <ambrosiogabe@gmail.com>
2021-08-15 16:42:50 -0500

    Update single include

--- single_include/logger/logger.h
+++ single_include/logger/logger.h
@@ -39,150 +39,25 @@ g_logger_level g_logger_get_level();
 #endif
 
 #ifdef GABE_LOGGER_IMPL
-#ifndef _WIN32
-#include <stdio.h>
-#include <varargs.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <chrono>
-
-void _g_logger_log(const char* filename, int line, const char* format, ...)
-{
-	if (g_logger_get_level() <= g_logger_level::Log)
-	{
-		printf("%s (line %d) Log: \n", filename, line);
-
-		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		char buf[20] = { 0 };
-		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
-		printf("[%s]: ", buf);
-
-		va_list args;
-		va_start(args, format);
-		vprintf(format, args);
-		va_end(args);
-
-		printf("\n");
-	}
-}
-
-void _g_logger_info(const char* filename, int line, const char* format, ...)
-{
-	if (g_logger_get_level() <= g_logger_level::Info)
-	{
-		printf("%s (line %d) Info: \n", filename, line);
-
-		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		char buf[20] = { 0 };
-		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
-		printf("[%s]: ", buf);
-
-		va_list args;
-		va_start(args, format);
-		vprintf(format, args);
-		va_end(args);
-
-		printf("\n");
-	}
-}
-
-void _g_logger_warning(const char* filename, int line, const char* format, ...)
-{
-	if (g_logger_get_level() <= g_logger_level::Warning)
-	{
-		printf("%s (line %d) Warning: \n", filename, line);
-
-		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		char buf[20] = { 0 };
-		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
-		printf("[%s]: ", buf);
-
-		va_list args;
-		va_start(args, format);
-		vprintf(format, args);
-		va_end(args);
-
-		printf("\n");
-	}
-}
-
-void _g_logger_error(const char* filename, int line, const char* format, ...)
-{
-	if (g_logger_get_level() <= g_logger_level::Error)
-	{
-		printf("%s (line %d) Error: \n", filename, line);
-
-		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		char buf[20] = { 0 };
-		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
-		printf("[%s]: ", buf);
-
-		va_list args;
-		va_start(args, format);
-		vprintf(format, args);
-		va_end(args);
-
-		printf("\n");
-	}
-}
-
-void _g_logger_assert(const char* filename, int line, int condition, const char* format, ...)
-{
-	if (g_logger_get_level() <= g_logger_level::Assert)
-	{
-		if (!condition)
-		{
-			printf("%s (line %d) Assertion Failure: \n", filename, line);
-
-			std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-			char buf[20] = { 0 };
-			std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
-			printf("[%s]: ", buf);
-
-			va_list args;
-			va_start(args, format);
-			vprintf(format, args);
-			va_end(args);
-
-			printf("\n");
-			exit(-1);
-		}
-	}
-}
-#endif
-#endif
-
-#ifdef GABE_LOGGER_IMPL
-
-static g_logger_level log_level = g_logger_level::All;
-
-void g_logger_set_level(g_logger_level level)
-{
-	log_level = level;
-}
-
-g_logger_level g_logger_get_level()
-{
-	return log_level;
-}
-
-#endif
-
-#ifdef GABE_LOGGER_IMPL
 #ifdef _WIN32
 #include <stdio.h>
 #include <varargs.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <chrono>
+#include <thread>
+#include <mutex>
 
 #include <Windows.h>
 #include <crtdbg.h>
+
+static std::mutex logMutex;
 
 void _g_logger_log(const char* filename, int line, const char* format, ...)
 {
 	if (g_logger_get_level() <= g_logger_level::Log)
 	{
+		std::lock_guard<std::mutex> lock(logMutex);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN);
 		printf("%s (line %d) Log: \n", filename, line);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
@@ -205,6 +80,7 @@ void _g_logger_info(const char* filename, int line, const char* format, ...)
 {
 	if (g_logger_get_level() <= g_logger_level::Info)
 	{
+		std::lock_guard<std::mutex> lock(logMutex);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
 		printf("%s (line %d) Info: \n", filename, line);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
@@ -227,6 +103,7 @@ void _g_logger_warning(const char* filename, int line, const char* format, ...)
 {
 	if (g_logger_get_level() <= g_logger_level::Warning)
 	{
+		std::lock_guard<std::mutex> lock(logMutex);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_RED);
 		printf("%s (line %d) Warning: \n", filename, line);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
@@ -249,6 +126,7 @@ void _g_logger_error(const char* filename, int line, const char* format, ...)
 {
 	if (g_logger_get_level() <= g_logger_level::Error)
 	{
+		std::lock_guard<std::mutex> lock(logMutex);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
 		printf("%s (line %d) Error: \n", filename, line);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
@@ -273,6 +151,7 @@ void _g_logger_assert(const char* filename, int line, int condition, const char*
 	{
 		if (!condition)
 		{
+			std::lock_guard<std::mutex> lock(logMutex);
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
 			printf("%s (line %d) Assertion Failure: \n", filename, line);
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
@@ -289,6 +168,145 @@ void _g_logger_assert(const char* filename, int line, int condition, const char*
 
 			printf("\n");
 			_CrtDbgBreak();
+			exit(-1);
+		}
+	}
+}
+#endif
+#endif
+
+#ifdef GABE_LOGGER_IMPL
+
+static g_logger_level log_level = g_logger_level::All;
+
+void g_logger_set_level(g_logger_level level)
+{
+	log_level = level;
+}
+
+g_logger_level g_logger_get_level()
+{
+	return log_level;
+}
+
+#endif
+
+#ifdef GABE_LOGGER_IMPL
+#ifndef _WIN32
+#include <stdio.h>
+#include <varargs.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+static std::mutex logMutex;
+
+void _g_logger_log(const char* filename, int line, const char* format, ...)
+{
+	if (g_logger_get_level() <= g_logger_level::Log)
+	{
+		std::lock_guard<std::mutex> lock(logMutex);
+		printf("%s (line %d) Log: \n", filename, line);
+
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		char buf[20] = { 0 };
+		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
+		printf("[%s]: ", buf);
+
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+
+		printf("\n");
+	}
+}
+
+void _g_logger_info(const char* filename, int line, const char* format, ...)
+{
+	if (g_logger_get_level() <= g_logger_level::Info)
+	{
+		std::lock_guard<std::mutex> lock(logMutex);
+		printf("%s (line %d) Info: \n", filename, line);
+
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		char buf[20] = { 0 };
+		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
+		printf("[%s]: ", buf);
+
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+
+		printf("\n");
+	}
+}
+
+void _g_logger_warning(const char* filename, int line, const char* format, ...)
+{
+	if (g_logger_get_level() <= g_logger_level::Warning)
+	{
+		std::lock_guard<std::mutex> lock(logMutex);
+		printf("%s (line %d) Warning: \n", filename, line);
+
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		char buf[20] = { 0 };
+		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
+		printf("[%s]: ", buf);
+
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+
+		printf("\n");
+	}
+}
+
+void _g_logger_error(const char* filename, int line, const char* format, ...)
+{
+	if (g_logger_get_level() <= g_logger_level::Error)
+	{
+		std::lock_guard<std::mutex> lock(logMutex);
+		printf("%s (line %d) Error: \n", filename, line);
+
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		char buf[20] = { 0 };
+		std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
+		printf("[%s]: ", buf);
+
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+
+		printf("\n");
+	}
+}
+
+void _g_logger_assert(const char* filename, int line, int condition, const char* format, ...)
+{
+	if (g_logger_get_level() <= g_logger_level::Assert)
+	{
+		if (!condition)
+		{
+			std::lock_guard<std::mutex> lock(logMutex);
+			printf("%s (line %d) Assertion Failure: \n", filename, line);
+
+			std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+			char buf[20] = { 0 };
+			std::strftime(buf, sizeof(buf), "%Y-%m-%d %I:%M:%S", std::localtime(&now));
+			printf("[%s]: ", buf);
+
+			va_list args;
+			va_start(args, format);
+			vprintf(format, args);
+			va_end(args);
+
+			printf("\n");
 			exit(-1);
 		}
 	}
