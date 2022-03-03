70f1271afb7391b4a87d486fd15f1cfa12955418
Gabe <ambrosiogabe@gmail.com>
2022-03-02 19:23:07 -0600

    Update docs and add license and dll support

--- single_include/cppUtils/cppUtils.hpp
+++ single_include/cppUtils/cppUtils.hpp
@@ -1,8 +1,116 @@
+/*
+ Do this:
+	 #define GABE_CPP_UTILS_IMPL
+ before you include this file in *one* C++ file to create the implementation.
+
+ // i.e. it should look like this in *one* source file:
+ #define GABE_CPP_UTILS_IMPL
+ #include "cppUtils.hpp"
+
+
+
+ LICENSE
+
+ Open Source, see end of file
+
+
+
+ DOCUMENTATION
+
+ This library is a single header library, inspired by the stb libraries. It
+ includes several utilities to do simple things like logging and 
+ memory tracking/heap corruption detection.Here is a list of the functions 
+ and some simple documentation For DLL info, look at the end of the documentation.
+
+
+
+ MEMORY TRACKER/HEAP CORRUPTION DETECTOR
+
+ BASIC USAGE
+
+ int main() 
+ {
+	g_memory_init(true, 12);
+	// Run my app
+	g_memory_dumpMemoryLeaks();
+ }
+
+ FUNCTIONS
+
+ g_memory_init(bool detectMemoryLeaks, uint16 bufferPadding = 5);
+  - detectMemoryLeaks   -- Set this to true to detect memory corruption/leaks. When this is set to
+						   false, this is simply a wrapper around std::malloc, std::realloc, etc.
+  - bufferPadding       -- This is how many extra bytes will be allocated at the start and end of
+						   each memory allocation. I advise that you set this to a very low
+						   number, like 1-2 bytes, if you plan on releasing your app with this turned on.
+
+ g_memory_dumpMemoryLeaks();
+
+ NOTE: Only memory allocated using this function will be tracked
+	g_memory_allocate(size_t numBytes)
+
+ NOTE: Call this on any memory allocated with the above function to properly track it
+	g_memory_free(void* memory)
+
+ NOTE: Only call this on memory that was allocated using the above function
+	g_memory_realloc(void* memory, size_t newSize)
+
+ Miscellaneous memory functions:
+	g_memory_compareMem(void* a, void* b, size_t numBytes)
+	g_memory_zeroMem(void* memory, size_t numBytes);
+	g_memory_copyMem(void* dst, void* src, size_t numBytes);
+
+
+
+ LOGGER
+
+ BASIC USAGE
+
+ All logging functions follow the format of printf, where you call a function
+ like:
+
+	 printf("Some string %s", "Some Format");
+
+ by calling it with a string, followed by a list of formatting options.
+
+ FUNCTIONS
+
+ These are all the logging options listed from least severe, to most severe:
+
+	g_logger_log(const char* message, ...format)
+	g_logger_info(const char* message, ...format)
+	g_logger_warning(const char* message, ...format)
+	g_logger_error(const char* message, ...format)
+	g_logger_assert(bool condition, const char* failureMessage, ...format)
+
+ Use this to restrict the messages to a particular level. This will log anything
+ at the preferred level or higher in severity:
+
+	g_logger_set_level(g_logger_level level)
+
+
+ DLL STUFF
+
+ If you want to use this library as part of a DLL, I have created a macro:
+
+	GABE_CPP_UTILS_API
+
+ that does nothing, but prefaces each forward declaration. Simply redefine this
+ to your preferred __declspec(dllimport) or __declspec(dllexport) before including this
+ file anywhere and it should just work out.
+*/
+
 // ===================================================================================
 // Headers
 // ===================================================================================
 #ifndef GABE_CPP_UTILS_H
 #define GABE_CPP_UTILS_H
+
+// Override this define to add DLL support on your platform
+#ifndef GABE_CPP_UTILS_API 
+#define GABE_CPP_UTILS_API 
+#endif
+
 #include <cstdint>
 #include <cstddef>
 
@@ -23,16 +131,16 @@ typedef uint64_t uint64;
 #define g_memory_realloc(memory, newSize) _g_memory_realloc(__FILE__, __LINE__, memory, newSize)
 #define g_memory_free(memory) _g_memory_free(__FILE__, __LINE__, memory)
 
-void* _g_memory_allocate(const char* filename, int line, size_t numBytes);
-void* _g_memory_realloc(const char* filename, int line, void* memory, size_t newSize);
-void _g_memory_free(const char* filename, int line, void* memory);
+GABE_CPP_UTILS_API void* _g_memory_allocate(const char* filename, int line, size_t numBytes);
+GABE_CPP_UTILS_API void* _g_memory_realloc(const char* filename, int line, void* memory, size_t newSize);
+GABE_CPP_UTILS_API void _g_memory_free(const char* filename, int line, void* memory);
 
-void g_memory_init(bool detectMemoryLeaks, uint16 bufferPadding = 5);
-void g_memory_dumpMemoryLeaks();
+GABE_CPP_UTILS_API void g_memory_init(bool detectMemoryLeaks, uint16 bufferPadding = 5);
+GABE_CPP_UTILS_API void g_memory_dumpMemoryLeaks();
 
-int g_memory_compareMem(void* a, void* b, size_t numBytes);
-void g_memory_zeroMem(void* memory, size_t numBytes);
-void g_memory_copyMem(void* dst, void* src, size_t numBytes);
+GABE_CPP_UTILS_API int g_memory_compareMem(void* a, void* b, size_t numBytes);
+GABE_CPP_UTILS_API void g_memory_zeroMem(void* memory, size_t numBytes);
+GABE_CPP_UTILS_API void g_memory_copyMem(void* dst, void* src, size_t numBytes);
 
 // ----------------------------------
 // Logging Utils
@@ -63,14 +171,14 @@ enum g_logger_level
 #endif
 
 
-void _g_logger_log(const char* filename, int line, const char* format, ...);
-void _g_logger_info(const char* filename, int line, const char* format, ...);
-void _g_logger_warning(const char* filename, int line, const char* format, ...);
-void _g_logger_error(const char* filename, int line, const char* format, ...);
-void _g_logger_assert(const char* filename, int line, int condition, const char* format, ...);
+GABE_CPP_UTILS_API void _g_logger_log(const char* filename, int line, const char* format, ...);
+GABE_CPP_UTILS_API void _g_logger_info(const char* filename, int line, const char* format, ...);
+GABE_CPP_UTILS_API void _g_logger_warning(const char* filename, int line, const char* format, ...);
+GABE_CPP_UTILS_API void _g_logger_error(const char* filename, int line, const char* format, ...);
+GABE_CPP_UTILS_API void _g_logger_assert(const char* filename, int line, int condition, const char* format, ...);
 
-void g_logger_set_level(g_logger_level level);
-g_logger_level g_logger_get_level();
+GABE_CPP_UTILS_API void g_logger_set_level(g_logger_level level);
+GABE_CPP_UTILS_API g_logger_level g_logger_get_level();
 
 #endif // GABE_CPP_UTILS_H
 
@@ -122,7 +230,7 @@ struct DebugMemoryAllocation
 static std::mutex memoryMtx;
 static std::vector<DebugMemoryAllocation> allocations;
 static bool trackMemoryAllocations = false;
-static const std::array<uint8, 8> specialMemoryFlags = { (uint8)'h', (uint8)'e', (uint8)'Y', (uint8)'G' , (uint8)'a', (uint8)'b', (uint8)'e', (uint8)'!'};
+static const std::array<uint8, 8> specialMemoryFlags = { (uint8)'h', (uint8)'e', (uint8)'Y', (uint8)'G' , (uint8)'a', (uint8)'b', (uint8)'e', (uint8)'!' };
 static uint16 bufferPadding = 5;
 
 void g_memory_init(bool detectMemoryErrors, uint16 inBufferPadding)
@@ -133,7 +241,7 @@ void g_memory_init(bool detectMemoryErrors, uint16 inBufferPadding)
 
 void* _g_memory_allocate(const char* filename, int line, size_t numBytes)
 {
-	if (trackMemoryAllocations) 
+	if (trackMemoryAllocations)
 	{
 		// In memory error tracking mode, I'll add sentinel values to the beginning and
 		// end of the block of memory to ensure it doesn't have any errors on free
@@ -142,10 +250,10 @@ void* _g_memory_allocate(const char* filename, int line, size_t numBytes)
 		// In debug mode we allocate 10 extra bytes, 5 before the block and 5 after. We can use these to detect
 		// Buffer overruns or underruns
 		void* memory = std::malloc(numBytes);
-		if (memory) 
+		if (memory)
 		{
 			uint8* memoryBytes = (uint8*)memory;
-			for (int i = 0; i < bufferPadding; i++) 
+			for (int i = 0; i < bufferPadding; i++)
 			{
 				memoryBytes[i] = specialMemoryFlags[i % specialMemoryFlags.size()];
 			}
@@ -273,7 +381,7 @@ void _g_memory_free(const char* filename, int line, void* memory)
 		else
 		{
 			iterator->references--;
-			
+
 			if (iterator->references == 0)
 			{
 				// Check to see if our special flags were changed. If they were, we have heap corruption!
@@ -290,7 +398,7 @@ void _g_memory_free(const char* filename, int line, void* memory)
 				memoryBytes = (uint8*)memory + iterator->memorySize - bufferPadding;
 				for (int i = 0; i < bufferPadding; i++)
 				{
-					if (memoryBytes[i] != specialMemoryFlags[i % specialMemoryFlags.size()]) 
+					if (memoryBytes[i] != specialMemoryFlags[i % specialMemoryFlags.size()])
 					{
 						g_logger_warning("Heap corruption detected. Buffer overrun in memory allocated from: '%s' line: %d", iterator->fileAllocator, iterator->fileAllocatorLine);
 						break;
@@ -708,7 +816,49 @@ void _g_logger_assert(const char* filename, int line, int condition, const char*
 			printf("\n");
 			exit(-1);
 		}
-	}
+}
 }
 #endif // DEFAULT_LOGGING_IMPL
 #endif // CPP_UTILS_IMPL
+
+/*
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE A - MIT License
+Copyright (c) 2022 Gabe Ambrosio
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE B - Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+*/
